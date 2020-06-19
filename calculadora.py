6b2d2410d9cb4257c91a679b70abf3085bc05881
MaxIbanez <m.ibaezmolina@uandresbello.edu>
2020-06-19 02:33:41 -0400

    calculadora v0.1

---
+++ calculadora.py
@@ -0,0 +1,85 @@
+
+"""
+atributos de la calculadora
+
+SUMAR
+RESTAR
+DIVIDIR
+MULTIPLICAR
+OBTENER PORCENTAJE
+OBTENER RAIZ
+POTENCIAS
+
+TOTAL = 0
+
+TOTAL = TOTAL + I
+
+TIPOS DE LENGUAJES
+
+- funcionales
+- orientados a objetos
+- secuenciales
+- lenguaje de etiqueta = html5, html.
+[2,4,6,8]
+[0,1,2,3]
+
+tamaño = 4
+indice = 0 -> 1 -> 2 -> 3 -> 4
+
+total = 0 -> 2 -> 6 -> 12 -> 20
+
+
+
+"""
+def Suma(listaSum):
+	total = 0
+	indice = 0
+	while(indice < len(listaSum)):		
+		total = total + lista[indice]
+		indice+=1
+	print (total)
+
+def Resta(listaRest):
+	total = 0
+	indice = 0
+	while(indice < len(listaRest)):
+		total = total - lista[indice]
+		indice+=1
+	print (total)
+
+
+
+
+def Division(div1,div2):
+	divi = div1/div2
+	print (divi)
+
+
+
+def Multiplicar(multi1,multi2):
+	multi = multi1*multi2
+	print (multi)
+
+
+
+
+
+
+ejemplo = [2,4,6,8]
+n1 = 10
+n2 = 5
+
+
+#Multiplicar(n1,n2)
+
+#Division(n1,n2)
+
+#Resta(ejemplo)
+
+#Suma(ejemplo)
+
+
+
+def calculadora():
+	pass
+
